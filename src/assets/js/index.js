8bc96f8a45ac8e71307cbd30fc2f8585c5045e11
Thomaz <122468659+Th0mzzz@users.noreply.github.com>
2025-06-05 00:43:49 -0300

    feat: botao volta para o topo da pagina

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -7,3 +7,20 @@ menuMobile();
 
 // Chamando o carousel
 carousel();
+
+const buttonUpPage = document.getElementById("buttonUpPage");
+
+buttonUpPage.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 500) {
+    buttonUpPage.style.bottom = "1rem";
+  } else {
+    buttonUpPage.style.bottom = "-100%";
+  }
+});
