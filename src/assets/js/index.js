8279b922c6ce799e13abdd1f8ab79b2724009f23
Thomaz <122468659+Th0mzzz@users.noreply.github.com>
2025-06-05 00:38:33 -0300

    feat: adicionando seções da landing page e js do carousel

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,9 @@
 // importando o menu mobile de outro arquivo
+import carousel from "./carousel.js";
 import menuMobile from "./menuMobile.js";
 
 // chamando a função do menu mobile
 menuMobile();
+
+// Chamando o carousel
+carousel();
